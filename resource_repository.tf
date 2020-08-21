9967d1e998024a52d306bf4f007765914f338f47
Chris Chalmers <chris.chalmers@it-consulting.co>
2020-08-22 00:08:11 +0100

    Updated to include descriptions and topics

--- resource_repository.tf
+++ resource_repository.tf
@@ -1,6 +1,7 @@
 resource "github_repository" "azure_iaas_bastion_windows" {
   name         = "Azure-IaaS-Bastion-Windows"
-
+  description  = "Provision Windows Server 2019 instance and dependant resources in Azure"
+  topics       = ["terraform", "azure", "windows"]
   private      = false
   has_issues   = false
   has_projects = false
@@ -10,7 +11,8 @@ resource "github_repository" "azure_iaas_bastion_windows" {
 
 resource "github_repository" "azure_paas_webapp_docker_compose_wordpress" {
   name         = "Azure-PaaS-WebApp-Docker-Compose-Wordpress"
-
+  description  = "Provision Azure Web App for Docker Compose to run multi-container MySQL and Wordpress within the app service"
+  topics       = ["terraform", "azure", "docker", "wordpress", "web-app"]
   private      = false
   has_issues   = false
   has_projects = false
@@ -20,7 +22,8 @@ resource "github_repository" "azure_paas_webapp_docker_compose_wordpress" {
 
 resource "github_repository" "azure_saas_dns_public_zone_m365_records" {
   name         = "Azure-SaaS-DNS-Public-Zone-M365-Records"
-
+  description  = "Host your public DNS records on an Azure DNS Zone"
+  topics       = ["terraform", "azure", "dns", "microsoft-365", "office-365"]
   private      = false
   has_issues   = false
   has_projects = false
@@ -30,7 +33,8 @@ resource "github_repository" "azure_saas_dns_public_zone_m365_records" {
 
 resource "github_repository" "github_organisation_repositories" {
   name         = "GitHub-Organisation-Repositories"
-
+  description  = "Manage your GitHub organisation repositories and team memberships"
+  topics       = ["terraform"]
   private      = false
   has_issues   = false
   has_projects = false
